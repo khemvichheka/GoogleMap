d19b07b62cda0b5dde79805e2cca1ce5fc8f680e
khemvichheka <khemvichheka2311@gmail.com>
2023-06-12 15:39:12 +0700

    MapCalculate1

--- lib/custom_marker_widget/assets.marker_screen.dart
+++ lib/custom_marker_widget/assets.marker_screen.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+///
+
 List<Map<String, dynamic>> data = [
   {
     'id': '1',
